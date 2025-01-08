aa365c45a8bea2df955fd3c6ac4f49c987e32a11
Baha-Alawneh <bahaalawneh07@gmail.com>
2025-01-08 16:34:33 +0200

    make some changes

--- config/socket.js
+++ config/socket.js
@@ -1,59 +1,87 @@
-import { WebSocketServer } from 'ws';
-import {  getAdmins } from "../models/users.js";
+import { WebSocketServer, WebSocket } from 'ws';
+import { getAdmins,findUserById,findUser } from "../models/users.js";
 
 export const wss = new WebSocketServer({ noServer: true });
 
-
 let clients = new Map();
-let admins = new Map();
-const adminsFromDB = await getAdmins();
-adminsFromDB.forEach(admin => {
+let admins = new Map(); 
+const initializeAdmins = async () => {
+  const adminsFromDB = await getAdmins();
+  adminsFromDB.forEach(admin => {
     admins.set(admin.username, { ws: null });
-});
+  });
+};
+initializeAdmins();
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, request) => {
   const clientId = `Client_${Math.random().toString(36).substring(7)}`;
-  clients.set(clientId, ws);
+  let username = null;
+  let isCurrentAdmin;
+
   console.log(`${clientId} connected`);
-  ws.send(JSON.stringify({ type: 'welcome', clientId, admins: Array.from(admins.keys()) }));
 
   ws.on("message", async (data) => {
     const message = JSON.parse(data);
-
+    if (message.type === "authenticate") {
+       const id = message.user_id;
+       isCurrentAdmin=message.isAdmin;
+       const user=await findUserById(id);
+       username = user[0].username;
+      if (user[0].role === "admin") {
+        console.log(`Admin ${user[0].username} authenticated.`);
+        admins.set(user[0].username, { ws });
+      } else {
+        console.log(`User ${user[0].username} authenticated.`);
+        clients.set(user[0].username, { ws });
+        ws.send(JSON.stringify({ type: 'welcome', id:"user"+id, role: 'client' }));
+      }
+    }
     if (message.type === "direct-message") {
-      const { from, to, content } = message;
+      const { from, to, content,isAdmin } = message;
+        const user=await findUser(from);
 
-      if (admins.has(to)) {
-        const admin = admins.get(to);
-        if (!admin.ws) {
-          admins.set(to, { ws });
+        if (user.length === 0) {
+          console.log(`User ${from} not found in the database.`);
+          ws.close();
         }
 
-        const adminWs = admins.get(to).ws;
-
+      console.log(`Direct Message from ${from} to ${to}`);
+      console.log(isAdmin);
+      if (isAdmin) {
+       console.log("Direct Message from admin:", from);
+       console.log("Direct Message to user:", to);
+        const clientWs = clients.get(to)?.ws;
+        console.log("found ws", clientWs);
+        if (clientWs && clientWs.readyState === WebSocket.OPEN) {
+          console.log("hi");
+          clientWs.send(JSON.stringify({
+            type: "direct-message",
+            from,
+            content
+          }));
+        }
+      } else {
+         console.log("hereeeee");
+        const adminWs = admins.get(to)?.ws;
         if (adminWs && adminWs.readyState === WebSocket.OPEN) {
-          adminWs.send(
-            JSON.stringify({
-              type: "direct-message",
-              from,
-              content,
-            })
-          );
+          console.log("sending direct-message from user:", from, "to", to);
+          adminWs.send(JSON.stringify({
+            type: "direct-message",
+            from,
+            content
+          }));
         }
       }
     }
   });
+
   ws.on("close", () => {
-    clients.delete(clientId);
-    console.log(`${clientId} disconnected`);
+    if (isCurrentAdmin && username) {
+      admins.set(username, { ws: null });
+      console.log(`Admin ${username} disconnected`);
+    } else if (username) {
+      clients.delete(username);
+      console.log(`${username} disconnected`);
+    }
   });
 });
-
-  wss.on("close", (ws) => {
-    admins.forEach((admin, username) => {
-      if (admin.ws === ws) {
-        admins.set(username, { ws: null });
-        console.log(`Admin ${username} disconnected`);
-      }
-    });
-  });
